c13589284c3e0d82b3a27370af4fa896f225cfb2
vinson <dagger1245653@gmail.com>
2021-11-28 08:48:27 -0800

    add nickname input and leaderboard display, along with other little stuffs.

--- photo-tagger/src/App.js
+++ photo-tagger/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, setState } from 'react';
+import React, {setState, useState, useEffect, useDebugValue} from 'react';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { collection, getDocs, getFirestore } from "firebase/firestore"; 
-import { async } from '@firebase/util';
+import { collection, getDocs, addDoc, getFirestore, query, orderBy, limit } from "firebase/firestore";
+import Leaderboard from './Leaderboard';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAj5DTk736HAORLfaFJaeWuUEVj8Zu-2C4",
@@ -13,6 +13,14 @@ const firebaseConfig = {
   appId: "1:136198387706:web:b0f38476d608fe46461643",
   measurementId: "G-JDJ44L6TST"
 };
+
+//intervalId is a local variable and is reset every single time you call the function!
+var sec = 0;
+function pad ( val ) { return val > 9 ? val : "0" + val; }
+let intervalId = setInterval(() => {
+  document.getElementById("seconds").innerHTML=pad(++sec%60);
+  document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+}, 1000);
 //remind to click head
 function App() {
   const db = getFirestore();
@@ -48,18 +56,79 @@ function App() {
       }
     });
     if (marks === 3) {
-      console.log('Win!')
+      clearInterval(intervalId);
+      erase();
+    }
+  }
+  function erase() {
+    const kirby = document.querySelector('#kirby');
+    kirby.classList.add('hide');
+    const bowser = document.querySelector('#bowser');
+    bowser.classList.add('hide');
+    const red = document.querySelector('#pokemonRed');
+    red.classList.add('hide');
+    const tag = document.querySelector('#tag');
+    tag.classList.add('hide');
+    const dropdown = document.querySelector('#dropdown');
+    dropdown.classList.add('hide');
+    const image = document.querySelector('#image');
+    image.classList.add('hide');
+    const scoreInput = document.querySelector('#scoreInput');
+    scoreInput.classList.remove('hide');
+  }
+  let preData = [];
+  let [data, setData] = useState([]);
+  const submit = async () => {
+    const minutes = document.querySelector('#minutes').textContent;
+    const seconds = document.querySelector('#seconds').textContent;
+    console.log(minutes);
+    console.log(seconds);
+    const error = document.querySelector('#errorMessage');
+    const input = document.querySelector('#name');
+    if (input.value.length === 0) {
+      error.textContent = 'The name entered is empty!';
+    } else if (input.value.length < 3) {
+      error.textContent = 'The name entered is too short!';
+    } else {
+      const db = getFirestore();
+		  const scoresRef = collection(db, "scoreboard");
+		  const q = query(scoresRef, orderBy("time"), limit(15));
+		  const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        preData.push(doc.data());
+				console.log('a')
+      });
+      setData(preData);
+      const scoreInput = document.querySelector('#scoreInput');
+      scoreInput.classList.add('hide');
+      error.textContent = '';
+      try {
+        await addDoc(collection(db, `scoreboard`), {
+          name: input.value,
+          time: minutes + seconds,
+        });
+        } catch (e) {
+          console.error("Error adding document: ", e);
+      }
     }
   }
   return (
-    <div>
+    <div lang="en">
+      <Leaderboard array={data} />
+      <div id="scoreInput" className="hide">
+        <h2 id="winHeader">Congratulations!</h2>
+        <p id="winPara">Please enter your name to publish your score!</p>
+        <p id="warning">Remember: it must be at least three characters long!</p>
+        <input type="text" id="name"></input>
+        <p id="errorMessage"></p>
+        <button onClick={submit} id="submit">Submit!</button>
+      </div>
+      <p id="timer"><span id="minutes"></span>:<span id="seconds"></span></p>
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet"/>
       <div className="crossOut material-icons hide" id="kirby">close</div>
       <div className="crossOut material-icons hide" id="bowser">close</div>
       <div className="crossOut material-icons hide" id="pokemonRed">close</div>
-      <div id="navbar">
-      </div>
       <div id="targetBox" className="hide">
         <div id="tag"></div>
         <div id="dropdown">
---
+++ photo-tagger/src/Leaderboard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Leaderboard(props) {
+	const { array } = props;
+	if (array.length !== 0) {
+		const leaderboard = document.querySelector('#leaderboard');
+		leaderboard.classList.remove('hide');
+	}
+	return (
+		<div lang="en" id="leaderboard" class="hide">
+			<h2>Leaderboard</h2>
+			<table id="scores">
+				{array.map(doc => {
+					return <tbody key={doc['name'] + doc['time']}>
+						<tr>
+							<td class="name">{doc['name']}</td>
+							<td>{doc['time'].substring(0, 2) + '.' + doc['time'].substring(2, doc['time'].length) + 's'}</td>
+						</tr>
+					</tbody>
+				})}
+			</table>
+		</div>
+	);
+}
+
+export default Leaderboard;
